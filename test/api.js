d11dcaafe80f0f4e58ff57f2c72e174db4a0b7c0
raghavsym <pr.raghavsingh@gmail.com>
2023-12-30 17:41:52 +0530

    Test cases commented out

--- test/api.js
+++ test/api.js
@@ -1,48 +1,48 @@
-const chai = require('chai');
-const request = require('supertest');
-const app = require('../src/config/server/server').default;
-const UserModel = require('../src/components/User/model').default;
+// const chai = require('chai');
+// const request = require('supertest');
+// const app = require('../src/config/server/server').default;
+// const UserModel = require('../src/components/User/model').default;
 // const ClientModel = require('../src/config/oauth/clientModel').default;
 // const TokenModel = require('../src/config/oauth/tokenModel').default;
 // const AuthCodeModel = require('../src/config/oauth/authCodeModel').default;
-chai.should();
+// chai.should();
 
-/**
- * API tests
- */
-// describe('API', () => {
-//     it('get user all cars', (done) => {
-//         request(app)
-//             .get('/v1/cars')
-//             .set('Authorization', `Bearer ${global.accessToken}`)
-//             .expect((res) => {
-//                 res.status.should.equal(200);
-//                 res.body.should.be.an('array');
-//             })
-//             .end(done);
-//     });
+// /**
+//  * API tests
+//  */
+// // describe('API', () => {
+// //     it('get user all cars', (done) => {
+// //         request(app)
+// //             .get('/v1/cars')
+// //             .set('Authorization', `Bearer ${global.accessToken}`)
+// //             .expect((res) => {
+// //                 res.status.should.equal(200);
+// //                 res.body.should.be.an('array');
+// //             })
+// //             .end(done);
+// //     });
 
-//     it('get availble cars for given set of dates', (done) => {
-//         const newUser = {
-//             "startDate": "2023-03-02",
-//             "endDate": "2023-03-04"
-//         }
+// //     it('get availble cars for given set of dates', (done) => {
+// //         const newUser = {
+// //             "startDate": "2023-03-02",
+// //             "endDate": "2023-03-04"
+// //         }
 
-//         request(app)
-//             .post('/v1/availability/getCarAvailability')
-//             .send(newUser)
-//             .set('Authorization', `Bearer ${global.accessToken}`)
-//             .expect((res) => {
-//                 res.status.should.equal(200);
-//                 res.body.should.be.an('array');
-//             })
-//             .end(done);
-//     });
-// });
+// //         request(app)
+// //             .post('/v1/availability/getCarAvailability')
+// //             .send(newUser)
+// //             .set('Authorization', `Bearer ${global.accessToken}`)
+// //             .expect((res) => {
+// //                 res.status.should.equal(200);
+// //                 res.body.should.be.an('array');
+// //             })
+// //             .end(done);
+// //     });
+// // });
 
-/**
- * clear database after tests
- */
+// /**
+//  * clear database after tests
+//  */
 // after(async () => {
 //     try {
         
--- test/fixtures/OAuthToken.js
+++ test/fixtures/OAuthToken.js
@@ -1,15 +1,15 @@
-const date = new Date();
+// const date = new Date();
 
-module.exports = {
-    accessToken: 'accessToken',
-    accessTokenExpiresAt: date.setSeconds(date.getSeconds() + 60 * 60),
-    refreshToken: 'refreshToken',
-    refreshTokenExpiresAt: date.setSeconds(date.getSeconds() + 60 * 60 * 24 * 24),
-    client: {
-        id: '6d052460baecf69ec3a76c82b1f69a4c36131d86',
-        grants: [
-            'authorization_code',
-            'refresh_token'
-        ]
-    }
-};
+// module.exports = {
+//     accessToken: 'accessToken',
+//     accessTokenExpiresAt: date.setSeconds(date.getSeconds() + 60 * 60),
+//     refreshToken: 'refreshToken',
+//     refreshTokenExpiresAt: date.setSeconds(date.getSeconds() + 60 * 60 * 24 * 24),
+//     client: {
+//         id: '6d052460baecf69ec3a76c82b1f69a4c36131d86',
+//         grants: [
+//             'authorization_code',
+//             'refresh_token'
+//         ]
+//     }
+// };
